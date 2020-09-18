0afbc7913667e9a333fc38d606a79b4f114fe3a7
Bryan Reyes <bryan_reyes@avansys.com.mx>
2020-09-18 11:42:26 -0500

    Basic tools

--- src/Generator/tools.py
+++ src/Generator/tools.py
@@ -1,6 +1,6 @@
 from os import path as p
 from os import makedirs
 
-def set_directory(path): makedirs(path,0o777) if not p.exists(path) else print(path + 'directory already exist')
+def set_directory(path:str): makedirs(path,0o777) if not p.exists(path) else print(path + 'directory already exist')
     
-def get_file(path): return open(path,"x") if not p.exists(path) else open(path,"a")
+def get_file(path:str): return open(path,"x") if not p.exists(path) else open(path,"a")
