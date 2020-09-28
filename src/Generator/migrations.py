99cbb0e41bdc873fc020842f36f0d7e35bc548cb
Bryan Reyes <bryan_reyes@avansys.com.mx>
2020-09-28 12:07:59 -0500

    Nombre de migracion y hora

--- src/Generator/migrations.py
+++ src/Generator/migrations.py
@@ -3,23 +3,25 @@ from os import makedirs
 from .tools import get_file
 from .tools import set_directory
 
+from datetime import datetime
 
 class migration_generator():
     
-    def __init__(self, model_name:str,fields:list):
+    def __init__(self, model_name:str,fields:list,app_name:str="app"):
         self.txt = []
         self.fields=fields
         self.model_name = model_name
+        self.app_name = app_name
         
-    
-    
-
     def __set_default_dependencies(self):
         self.txt.append("<?php\n")
         self.txt.append("use Illuminate\Support\Facades\Schema;\n")
         self.txt.append("use Illuminate\Database\Schema\Blueprint;\n")
         self.txt.append("use Illuminate\Database\Migrations\Migration;\n")
         self.txt.append("\n")
+        self.txt.append("class CreateMainTablesMigration extends Migration {\n")
+        
+    def __closeFile(self): self.txt.append("\n}\n")
     
     def _set_fields(self,default_fields:bool=True)->list:
         arra_txt = []
@@ -52,16 +54,14 @@ class migration_generator():
         
         return model_file
         
-    def create(self, path="./result", default_fields=False):
+    def create(self, path:str="./result", default_fields:bool=False,close:bool = False):
         
         set_directory(path)
-        path= path+"/create_main_tables_migration.php"
+        path= path+"/"+datetime.now().strftime("%Y_%m_%d_%H%M%S")+"_create_main_tables_migration.php"
         file = self.set_file(path)
-        self.txt.append("//"+str(self.model_name)+"_model \n")
-        self.txt.append("class Create"+self.model_name+" extends Migration \n{\n")
         
+        self.txt.append("//"+str(self.model_name)+"_model \n")
         #up function
-        self.txt.append("\tpublic function up()\n\t{\n")#open
         self.txt.append("\t\tSchema::create('"+self.model_name+"', function (Blueprint $table) {\n")
         self.txt.extend(self._set_fields())
         #print(self.txt)
@@ -73,8 +73,8 @@ class migration_generator():
         self.txt.append("\t\tSchema::dropIfExists('"+self.model_name+"');\n")
         self.txt.append("\n\t}\n") #Close
 
-        self.txt.append("\n}\n") #Class Close
 
+        if close: self.__closeFile()
         file.writelines(self.txt)
         pass
     pass
@@ -84,18 +84,9 @@ class Migrations_Generator(object):
         
         for m in app["tables"]:
             print("Cooking "+m["name"]+" model ...")
-            model_ = migration_generator(m["name"],m['fields'])
-            model_.create(path= out_path+project_name+"/database/migrations",default_fields=True)
+            migration_generator(m["name"],m['fields']).create(path= out_path+project_name+"/database/migrations",default_fields=True,close = (m == app["tables"][-1]))
             #model_.set_fields(m["fields"])
-        try:
-            pass
-        except Exception as e:
-            print("Models:\n")
-            print(e)
-            pass
-                
         
-    pass
 
 if __name__ == "__main__":
     
